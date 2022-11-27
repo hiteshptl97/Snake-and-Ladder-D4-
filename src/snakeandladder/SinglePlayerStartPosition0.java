524928ef277b571bea047c46745bc7e0e14b6af3
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-27 13:56:02 +0530

    player start with position 0

---
+++ src/snakeandladder/SinglePlayerStartPosition0.java
@@ -0,0 +1,18 @@
+package snakeandladder;
+
+public class SinglePlayerStartPosition0 {
+
+
+//This is a Snake and Ladder game simulation program where a single player is playing, starting from zero.
+
+
+    SinglePlayerStartPosition0() {System.out.println("Welcome to Snake and Ladder Simulation \n");}
+
+        public static void main(String[] args) {
+            SinglePlayerStartPosition0 a  = new SinglePlayerStartPosition0();
+            int startingPosition = 0;
+            System.out.println("Starting Position of the Single Player is : "+startingPosition);
+        }
+
+    }
+
